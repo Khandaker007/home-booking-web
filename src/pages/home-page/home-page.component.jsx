b50a77926b397c12cd18673e96dc36bfbc3eb0cc
ninad <na.ninad60@gmail.com>
2021-07-05 04:21:26 +0600

    footer section complete

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -94,7 +94,17 @@ const HomePage = () =>{
             </section>
             
             <footer className="footer">
-                footer
+                <ul className="nav">
+                    <li className="nav__item"><a href="#" className="nav__link">Find your dream house</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+                </ul>
+                <p className="copyright">
+                    &copy; Copyright 2021 by Khandaker Nabiul. Design by other person.
+                </p>
             </footer>
         </div>
 )}
